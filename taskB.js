cc433df97b161e1fa6e2d333a3fd79054677ad48
Pauliukovich Artsiom <artem.pavl@protonmail.com>
2021-09-01 16:54:48 +0300

    task2

--- taskB.js
+++ taskB.js
@@ -1,31 +1,61 @@
 function solve(input) {
   if (input) {
     if (typeof input === 'number' && input > 0 && input <= 10 ** 18) {
-      const result = [];
       let teams = 1,
-          wars = 0;
+          wars1 = 0,
+          wars2 = input,
+          result = [];
 
       for (let i = 1; i <= input; i++) {
-        if (wars < input) {
-          wars += Math.pow(2, i) / 2;
+        if (wars1 < input) {
+          wars1 += Math.pow(2, i) / 2;
           teams *= 2;
         }
         
-        /* if (i % 2) {
-          let wars = 0;
+        if (wars2 % 2 !== 0) {
+          if (wars2 <= 1) {
+            break;
+          }
 
-          for (let val = 1; val < i; val++) {
-            wars += i - val;
+          let teams = 1,
+              wars = 0;
+          
+          while (wars < input) {
+            wars = 0;
+            teams += 2;
+
+            for (let i = 1; i < teams; i++) {
+              wars += teams - i;
+            }
+
+            if (wars === input) {
+              result.push(teams);
+            } else {
+              let valueTeams = teams;
+
+              for (let i = wars; i < input; i += valueTeams / 2) {
+                valueTeams *= 2;
+
+                if (i + valueTeams / 2 === input) {
+                  result.push(valueTeams);
+                  break;
+                }
+              }
+            }
           }
-          console.log(wars);
-          if (wars === input || input - wars % 2 === 0) {
-            result.push(i);
-          }
-        } */
+
+          wars2 = 0;
+        } else {
+          wars2 /= 2;
+        }
       }
       
-      result.push(input === wars ? teams: 0);
-      console.log(result);
+      if (input === wars1) {
+        result.push(teams);
+      }
+      
+      return !result.length ? -1 : 
+        result.sort((a, b) => a - b).reduce((sum, a) => sum + (a ? '\n' + a : '')); 
     } else {
       return 'Invalid input data';
     }
@@ -34,7 +64,13 @@ function solve(input) {
   return 'No input data';
 }
 
+console.log(solve(63));
+console.log(solve(630));
+console.log(solve(636));
 console.log(solve(15));
+console.log(solve(3));
+console.log(solve(10));
+console.log(solve(12));
 
 /* const fs = require('fs')
 const input = fs.readFileSync(0, 'utf-8');
